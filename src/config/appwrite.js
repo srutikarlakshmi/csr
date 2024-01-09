862bb13dbd6ad8244ba1563959164a048b97e88d
srutikarlakshmi <srutikarlakshmi@gmail.com>
2024-01-09 23:45:01 +0530

    Appwrite DB

--- src/config/appwrite.js
+++ src/config/appwrite.js
@@ -2,7 +2,7 @@ import {Client, Account, Databases} from "appwrite";
 
 const client = new Client();
 
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject("657bf4fb5b0ffd89472c");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("658fb3f1f1ec858f4903");
 
 export const account = new Account(client);
-export const database = new Databases(client, "658bb17ab62e4400ccc2");
+export const database = new Databases(client);
